ef2c7fa649beb4ca7888aa7d30212c08aa89a964
Maxim Shiryaev <maksim155555@gmail.com>
2019-01-17 21:26:50 +0200

    123

---
+++ index.js
@@ -0,0 +1,29 @@
+// var i = 0;
+// while (i < 10) {
+//     i++;
+//     debugger;
+// }
+//выводить пока не будет 9
+// var a = 9;
+// var b = prompt("try");
+// while (a != b) {
+//     var b = prompt("try?");
+// } //выводить пока не будет 9
+// var a = 9;
+
+// while (true) {
+//     var b = prompt("try");
+//     if (a != b) {
+//         break;
+//     }
+// }
+// var a = 9;
+// var b = prompt("try");
+// var i;
+// ++i;
+// if (a! > b) {
+//     var b = prompt("try?");
+//     console.log("win");
+
+// }
+// alert(a == b ? "win" : "lose")
